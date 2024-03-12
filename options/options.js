1a2c304ab711ed44a0b7d694262f6d66ee512df7
Crowdbotics <team@crowdbotics.com>
2024-03-12 05:08:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "digital-closet-46953",
+  url: "https://digital-closet-46953.botics.co",
+  api: "https://digital-closet-46953.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
